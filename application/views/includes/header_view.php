1187ca59c20b0acabcd633a18db51d661e83bfc4
azurast <azura.sakan@student.umn.ac.id>
2019-05-09 13:53:53 +0700

    small fixes
    - change base controller to controller/home
    - removed binary files
    - able to click login & signup
    - for now change sign up using username
    - delete remember me button on sign in

---
+++ application/views/includes/header_view.php
@@ -0,0 +1,74 @@
+<body class="bg-secondary g-sidenav-show g-sidenav-pinned" data-gr-c-s-loaded="true">
+    <!-- Navbar -->
+    <nav id="navbar-main" class="navbar navbar-horizontal navbar-transparent navbar-main navbar-expand-lg navbar-light bg-gradient-primary">
+        <a class="navbar-brand" href="#">
+            <img src="argon\assets\img\logo\Logo.png" width="150" height="50" alt="">
+        </a>
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-lable="Toggle Navigation"><span class="navbar-toggler-icon"></button>
+            <div id="navbar-collapse" class="navbar-collapse navbar-custom-collapse collapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="#"><span class="nav-link-inner--text text-white">Home</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="#"><span class="nav-link-inner--text text-white">About Us</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url('home/sign_up') ?>"><span class="nav-link-inner--text text-white">Sign Up</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo base_url('home/sign_in') ?>"><span class="nav-link-inner--text text-white">Sign In</span></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <ul class="nav justify-content-center mt-3 mb-3">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Tops</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Bottoms</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Dresses</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Jumpsuits</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Accessories</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Toys</a>
+      </li>
+    </ul>
+    <!-- Content -->
+    <div class="main-content">
+        <!-- Header -->
+        <!-- <div class="header bg-gradient-info py-7 py-lg-8 pt-lg-9">
+            <div class="container">
+                <div class="header-body text-center mb-5">
+                    <div class="row justify-content-center">
+                        <div class="col-xl-6 col-lg-6 col-md-8 px-5">
+                            <!-- Brand Logo -->
+                            <!-- <img src="<?php echo base_url('argon\assets\img\logo\Logo.png');?>" alt="Infinistyle"> -->
+                            <!-- Clothing Categories -->
+                            <!-- <ul class="nav nav-pills nav-fill flex-column flex-sm-row mt-3" id="tabs-text" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link mb-sm-3 mb-md-0 active" id="tabs-text-1-tab" data-toggle="tab" href="#tabs-text-1" role="tab" aria-controls="tabs-text-1" aria-selected="true">Tops</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-text-2-tab" data-toggle="tab" href="#tabs-text-2" role="tab" aria-controls="tabs-text-2" aria-selected="false">Bottoms</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-text-3-tab" data-toggle="tab" href="#tabs-text-3" role="tab" aria-controls="tabs-text-3" aria-selected="false">Dresses</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs-text-4-tab" data-toggle="tab" href="#tabs-text-4" role="tab" aria-controls="tabs-text-4" aria-selected="false">Accessories</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>-->
+        <!-- Page Content -->
+        <!-- Carousel -->
+        <!-- Shop Now -->
+        <!-- Product Cards -->
+    </div>
+</body>
