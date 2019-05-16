3df1d24403ce491ba7658ca6f99289540269e71c
azurast <azura.sakan@student.umn.ac.id>
2019-05-16 15:37:27 +0700

    ganti warna sign up & sign in + di bold text nav nya

--- application/views/includes/shop/footer.php
+++ application/views/includes/shop/footer.php
@@ -2,7 +2,7 @@
 <div class="container">
     <footer class="footer bg-default">
         <div class="copyright text-center text-muted">
-            &copy; 2019 <a href="#" class="font-weight-bold" target="_blank">Infinistyle</a>
+            &copy; 2019 <a href="#" class="font-weight-bold text-success" target="_blank">Infinistyle</a>
         </div>
         <!-- <div class="row align-items-center justify-content-xl-between"> -->
 
